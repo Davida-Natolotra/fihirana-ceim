1c51793f453a6bd1d0d978c9e41c545b8ae92861
Davida <r.davida.natolotra@gmail.com>
2025-06-26 21:50:53 +0300

    Update security environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,23 +14,14 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { enableIndexedDbPersistence } from 'firebase/firestore';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { getFrenchPaginatorIntl } from './paginator-intl-fr';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'fihirana-ceim',
-        appId: '1:173456515152:web:398265d51fa299f0b1b155',
-        storageBucket: 'fihirana-ceim.firebasestorage.app',
-        apiKey: 'AIzaSyC9waL0K05WYeyCksoDjYNlk6HZ-hQ-tcU',
-        authDomain: 'fihirana-ceim.firebaseapp.com',
-        messagingSenderId: '173456515152',
-        measurementId: 'G-JNY9FV3LM2',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => {
       const firestore = getFirestore();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'fihirana-ceim',
+    appId: '1:173456515152:web:398265d51fa299f0b1b155',
+    storageBucket: 'fihirana-ceim.firebasestorage.app',
+    apiKey: 'AIzaSyC9waL0K05WYeyCksoDjYNlk6HZ-hQ-tcU',
+    authDomain: 'fihirana-ceim.firebaseapp.com',
+    messagingSenderId: '173456515152',
+    measurementId: 'G-JNY9FV3LM2',
+  },
+};
